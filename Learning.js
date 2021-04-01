7cec1f6b157e31a0e3c99b995e8f72d9e2360a63
student <rahmafaizyusuf@gmail.com>
2021-04-01 12:18:34 +0300

    first commit

---
+++ Learning.js
@@ -0,0 +1,81 @@
+// Strings, Numbers, Boolean, null, undefined
+
+const name="Rahma";
+const isBad=true;
+const x=null;
+const y=undefined;
+let z;
+
+console.log(typeof name);
+console.log("");
+
+//Loops
+
+let searching=0;
+while(searching>50){                              //while loop
+  console.log("app is still loading");
+  
+}
+ console.log("");
+
+       // for..of loop
+
+       var cities=["Tokyo","Japan","Dubai","Spain","Rassia","France"];
+       for(const n of cities){                  // arrays
+          console.log(n);
+       }
+       console.log("");
+
+       // for..in loop
+
+       const vehicle={
+           make:"Toyota",
+           model:"Mazda",
+           color:"Black",
+           capacity:8,
+       }
+       
+
+          for(const n in vehicle){           // objects
+              console.log(vehicle[n]);     // obj is not a fun                                                                   
+              //console.log(n);
+          }
+
+
+
+/*
+          var a="Happy Birthday, Chantal !";
+          var b="You are now 21 !";
+          var c="Enjoy your day !";
+          console.log("");
+
+          console.log(a);
+          console.log(b);
+          console.log(c);
+          console.log("");
+
+          // Lets print a cake
+
+          console.log("   ,,,,,   ");
+          console.log("   |||||   ");
+          console.log(" ========= ");
+          console.log("@@@@@@@@@@@");
+          console.log("{~@~@~@~@~}");
+          console.log("@@@@@@@@@@@");
+          console.log("");
+*/
+          
+
+          
+
+          
+
+
+
+       
+
+
+
+
+
+        
