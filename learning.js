7a4e2eb1e694be40b7c0698557c5546270e64cef
student <rahmafaizyusuf@gmail.com>
2021-04-04 18:22:20 +0300

    first commit

---
+++ learning.js
@@ -0,0 +1,42 @@
+// for..of loop
+
+       var cities=["Tokyo","Japan","Dubai","Spain","Rassia","France"];
+       for(const n of cities){                  // arrays
+          console.log(n);
+       }
+       console.log("");
+
+       // for..in loop
+
+       const vehicle={
+           make:"Toyota",
+           model:"Mazda",
+           color:"Black",
+           capacity:8,
+       }
+       
+
+          for(const n in vehicle){           // objects
+              console.log(vehicle[n]);                                                                
+              //console.log(n);
+          }
+
+
+
+        
+
+          
+
+          
+
+          
+
+
+
+       
+
+
+
+
+
+        
